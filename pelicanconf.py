a4f45a8d05ff6222e2bc1ac7e89e328f95eb0a76
Little-Captain <littlecaptain@foxmail.com>
2018-05-09 11:14:40 +0800

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Little Captain'
+SITENAME = "Little Captain's Blog"
+SITEURL = ''
+
+PATH = 'articles'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('My Github', 'https://github.com/Little-Captain'),)
+
+# Social widget
+# SOCIAL = (('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
